9e3634b1ca63ac3643e085139789e690ecfd5043
donpablohdf <pabloalegrejuan@gmail.com>
2022-09-27 11:35:08 +0000

    Salida

--- app.js
+++ app.js
@@ -15,30 +15,26 @@ const EXTENSION = ".com";
 
 
 function crearNombreDeDominio(arr1,arr2,arr3){
-var nombresDominio = new Array();
-var recorrePrimerArray = arr1.map(function(palabra){
-  
-  var recorreSegundoArray= arr2.map(function(segundaPalabra){
-   
-      var recorreTercerArray = arr3.map(function(tercerPalabra){
-          // inserta los datos en el array "nombresDominio"
-          nombresDominio.push (palabra + segundaPalabra + tercerPalabra + EXTENSION);               
-      })
-  })
-  
-})
-  return nombresDominio;
+    var nombresDominio = new Array();
+    var recorrePrimerArray = arr1.map(function(palabra){  
+      var recorreSegundoArray= arr2.map(function(segundaPalabra){   
+          var recorreTercerArray = arr3.map(function(tercerPalabra){
+              // inserta los datos en el array "nombresDominio"
+              nombresDominio.push (palabra + segundaPalabra + tercerPalabra + EXTENSION);               
+          })
+      })      
+    })
+    return nombresDominio;
 }
 
 function agregarElementosID(arr4){ 
-  var elementos ="";
-  arr4.forEach(function(data){
-    elementos = elementos + " " + data;    
-  })
-  return elementos;
+    var elementos ="";
+    arr4.forEach(function(data){
+      elementos = elementos + " " + data;    
+    })
+    return elementos;
 }
-  let salidaID= agregarElementosID(crearNombreDeDominio(PRONOUN, ADJ, NOUN));
-  console.log(salidaID);
-  document.getElementById("elementos").innerHTML = salidaID;
-
-//console.log(crearNombreDeDominio(PRONOUN, ADJ, NOUN));
+// salida al ID
+let salidaID= agregarElementosID(crearNombreDeDominio(PRONOUN, ADJ, NOUN));
+console.log(salidaID);
+document.getElementById("elementos").innerHTML = salidaID;
